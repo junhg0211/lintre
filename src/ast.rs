93a3c450b61136d19c3c58685ea796d9ae4b95e6
Sch <junhg0211@gmail.com>
2025-04-28 23:33:37 +0000

    galeop

--- src/ast.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum Expr {
-    Word(String),
-    Words(Vec<Expr>),
-    Function(Vec<String>, Box<Expr>),
-    Define(String, Box<Expr>),
-    Sequence(Vec<Expr>),
-    Paren(Box<Expr>),
-}
